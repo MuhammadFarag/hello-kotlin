19dfc3b90027ca1ea7ac2f7bae0f8b90197f4c0c
Muhammad Farag <muhammadfarag@gmail.com>
2018-05-04 21:06:24 -0400

    Custom getter.

--- src/com/mfarag/learn/kotlin/HelloKotlin.kt
+++ src/com/mfarag/learn/kotlin/HelloKotlin.kt
@@ -1,8 +1,15 @@
 package com.mfarag.learn.kotlin
 
+import java.time.LocalDate
+
 fun main(args: Array<String>) {
-    val person = Person("Kotlin", 7)
-    println("Hello ${person.name}!")
+    val person = Person("Kotlin", 2011)
+    println("Hello ${person.name}, you are ${person.age} years old now!")
 }
 
-class Person(val name: String, ageInYears: Int)
+class Person(val name: String, private val yearOfBirth: Int) {
+    val age: Int
+        get() {
+            return LocalDate.now().year - yearOfBirth
+        }
+}
